12ddcd92cb59cf8c23413d1ec36eb35d16d5a46a
Bedirhan Ayik <denzlit@yahoo.com>
2021-02-12 03:39:44 +0100

    Initial commit

---
+++ website/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bedirhan Ayik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+<body>
+
+<h1>Git yuh Git yuh Git Yuh</h1>
+<p>This is a paragraph.</p>
+
+
+
+</body>
+</html>
